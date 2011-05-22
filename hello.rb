88672fdc6b9d1c9b24863f3e7359e53999c5f4c1
Uri Gorelik <uri.gore@gmail.com>
2011-05-21 20:32:14 -0400

    Added a lot of shit

--- hello.rb
+++ hello.rb
@@ -1,12 +1,33 @@
-require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  @title = "Uri's Page"
+  @title = "Uri's Home"
+  @header = "Stuff"
   erb :index
 end
 
 get '/mac' do
-  @title = "Mac Tips"
+  @title = "Windows to Mac"
+  @header = "Coming to Mac from Windows"
   erb :mac
+end
+
+get '/resume' do
+  @title = "Uri's Resume"
+  erb :resume
+end
+
+get '/projects' do
+  @title = "Uri's Projects"
+  erb :projects
+end
+
+get '/portfolio' do
+  @title = "Uri's Portfolio"
+  erb :portfolio
+end
+
+get '/test' do
+  @title = "Uri's Portfolio"
+  erb :test
 end
