6947bb2be5dfcbbd0111b7ce0f8b12cbbfa43f66
Uri Gorelik <uri.gore@gmail.com>
2011-11-03 13:44:50 -0400

    Added to git

---
+++ hello.rb
@@ -0,0 +1,87 @@
+require 'sinatra'
+
+set :markdown, :layout_engine => :haml, :layout => layout
+
+helpers do
+  
+  
+  def link (name,url)
+    @output = ""
+    @output <<"<p class='link'><a href='http://#{url}' target='_blank'>#{name}</a></p>"
+    @output
+  end
+  
+  def image (name,url,size=nil,float=nil)
+    @output = ""
+    
+    if (float.nil?)
+      @output << "<p>"
+    else
+      @output << "<p style=' display:inline;'>"
+    end
+    
+    @output << "<a href='/images/#{url}' target='_blank'>"
+
+		if (size.nil?)
+			@output << "<img src='/images/#{url}' alt='#{name}' title='#{name}'>"
+		else
+			@output << "<img width='#{size}%' src='/images/#{url}' alt='#{name}' title='#{name}'>"
+		end
+    @output << "</a>"
+    @output << "</p>"
+    @output
+  end    
+
+  
+  
+end
+
+
+get '/' do
+  @title = "Uri's Home"
+  haml :index
+end
+
+get '/resume' do
+  @title = "Uri's Resume"
+	haml :resume
+end
+
+get '/projects' do
+  @title = "Uri's Projects"
+  haml :projects
+end
+
+get '/portfolio' do
+  @title = "Uri's Portfolio"
+  haml :portfolio
+end
+
+get '/contact' do
+  @title = "Contact"
+  @header = @title
+  haml :contact
+end
+
+get '/pages' do
+  @title = "Pages"
+  @header = "Pages"
+  haml :pages
+end
+
+get '/pages/mac' do
+  @title = "Windows to Mac"
+  @header = "Coming to Mac from Windows"
+  haml :mac
+end
+
+
+get '/pages/asteroids' do
+  @title = "Asteroids!"
+  @header = @title
+  haml :asteroids
+end
+
+
+
+
